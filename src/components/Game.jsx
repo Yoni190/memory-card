143f591c0f43d4bfa7d4e8d005458af24711e604
Yoni190 <yfisuhu@gmail.com>
2026-03-28 09:43:05 +0300

    Create Game component

---
+++ src/components/Game.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Game = () => {
+  return (
+    <div>
+        <header>
+            <h1>Memory Card</h1>
+            <small>Don't click on the pictures more than once</small>
+        </header>
+    </div>
+  )
+}
+
+export default Game
