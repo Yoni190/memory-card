d4259a52ad4d12763efc28a05dd935a84e96e8d6
Yoni190 <yfisuhu@gmail.com>
2026-03-28 10:50:41 +0300

    Display two pokes

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,12 +1,51 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 
 const Game = () => {
+
+  const [pokeData, setPokeData] = useState([])
+  const pokemons = ['ditto', 'pikachu']
+  const API_URL = 'https://pokeapi.co/api/v2/pokemon'
+
+  useEffect(() => {
+    let isMounted = true
+
+    const fetchPokes = async() => {
+      try {
+        pokemons.forEach(async (pokemon) => {
+          const res = await axios.get(`${API_URL}/${pokemon}`)
+          
+          if(isMounted) {
+            setPokeData(pokeData => [...pokeData, res.data])
+          }
+          
+        })
+      } catch (error) {
+        console.log(error)
+      }
+      
+    }
+
+    fetchPokes()
+
+    return () => {
+      isMounted = false
+    }
+    
+  }, [])
+
+  
   return (
     <div>
         <header>
             <h1>Memory Card</h1>
             <small>Don't click on the pictures more than once</small>
         </header>
+
+        {pokeData?.map((poke, index) => (
+          <img src={poke?.sprites?.front_default} alt="" key={index} />
+        ))}
+        
     </div>
   )
 }
